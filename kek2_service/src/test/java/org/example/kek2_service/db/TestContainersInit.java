1a5b23eb7ce35afb43474f6a1bd7c4f331c18060
dvernov1221 <archimg1221@gmail.com>
2025-03-03 22:10:15 +0300

    comm mewssage

--- kek2_service/src/test/java/org/example/kek2_service/db/TestContainersInit.java
+++ kek2_service/src/test/java/org/example/kek2_service/db/TestContainersInit.java
@@ -1,8 +1,13 @@
 package org.example.kek2_service.db;
 
 
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 public class TestContainersInit {
+    @Container
+    public static final PostgreSQLContainer<?> POSTGRES_CONTAINER =
+            new PostgreSQLContainer<>("postgres:latest");
 }
